aeb0b35bd3f1076337a446f386fbfaaf32594e17
AlexRG03 <alex.rodriguez.garcia@estudiantat.upc.edu>
2022-04-30 01:13:36 +0200

    nn

--- Àlex.py
+++ Àlex.py
@@ -1,18 +1,31 @@
-# Cosas a hacer
-## Pendiente
-- [ ] Contador de los puntos que tenemos y de los enemigos
-- [ ] Que casillas nos dan el máximo aumento de puntos(Valor de cada casilla)
-- [ ] Que casillas resta el máximo número de puntos(Valor de cada casilla)
+"""Estratègia de prova"""
 
-## Features
-- [ ] Comunicación de los movimientos entre unidades
-- [ ] Grafo de todas las posiciones con pesos
-## Estrategias
-## Probelmas
-- [ ] Cuantos puntos de media serían necesarios para ganar
-- [ ] Como es la mejor manera de interconectar conjuntos
-- [ ] En que posición queremos que esten nuestras fichas de "normal"
 
+for w in map.workers {
+  #Logic to check worker placement
+}
+
+for x in 0..40 {
+    for y in 0..40 {
+        if map[x][y] == Tile::EMPTY {
+            # more logic
+          }
+        #other logic
+      }
+  }
+
+for
+  w in 0..8 {
+    let r = (rand() % 4).abs();
+    switch r{
+          0 = > worker(w).move_up(),
+          1 = > worker(w).move_down(),
+          2 = > worker(w).move_right(),
+          3 = > worker(w).move_left(),
+      }
+
+      info(`worker ${w} finished`);
+  }
 
 """Funció que retorna un vector amb la puntuació de cada color:
 Puntuació=[verd, groc, vermell, blau]"""
