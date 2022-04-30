80931acde7b913e52a6052b9dd781ec57f17021c
AlexRG03 <alex.rodriguez.garcia@estudiantat.upc.edu>
2022-04-30 03:27:48 +0200

    n

--- LoQueLeGustaAHector.js
+++ LoQueLeGustaAHector.js
@@ -1,17 +1,1 @@
 
-for w in 0..8 {
-    let x=worker(w).x;
-    let y=worker(w).y;
-    if (y%2).abs==1{
-      worker(w).move_right();
-      if x==39 {
-        worker(w).move_down();
-      }
-    } else {
-      worker(w).move_left();
-      if x==0 {
-        worker(w).move_down();
-      }
-    }
-      info(`worker ${w} finished`);
-  }
