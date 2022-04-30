a806c4f49aaf6073c5dcfa0770001ac701537328
AlexRG03 <alex.rodriguez.garcia@estudiantat.upc.edu>
2022-04-30 02:54:49 +0200

    Update LoQueLeGustaAHector.js

--- LoQueLeGustaAHector.js
+++ LoQueLeGustaAHector.js
@@ -2,7 +2,7 @@
 for w in 0..8 {
     let x=worker(w).x;
     let y=worker(w).y;
-    if (y%2).abs==1{
+    if (y%2).abs()==1{
       worker(w).move_right();
       if x==39 {
         worker(w).move_down();
